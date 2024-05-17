f4649e79bd8361e0860e5f0955dda7d1486a78f4
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-05-17 14:23:15 +0300

    Add files via upload

---
+++ СТРУКТУРЫ/ConsoleApp1/MyClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        //1
+        //struct MyStruct
+        //{
+        //    public string change;
+        //}
+
+        //class MyClass1
+        //{
+        //    public string change;
+        //}
+
+
+        //2
+        struct Notebook
+    {
+        // Поля структуры
+            string Model; 
+            string Proizvoditel;
+            double Price;
+
+        public Notebook(string model, string proizvoditel, double price)
+        {
+            this.Model = model;
+            this.Proizvoditel = proizvoditel;
+            this.Price = price;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Модель:{Model}\nПроизводитель:{Proizvoditel}\nЦена:{Price}");
+        }
+        
+    }
+
+        
+
+
+
+}
---
+++ СТРУКТУРЫ/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //1
+            //MyClass1 myclass = new MyClass1();
+            //myclass.change = "изменино";
+            //MyStruct myStruct = new MyStruct();
+            //myStruct.change = "изменино";
+            //ClassTacer(myclass);
+            //StruktTacer(myStruct);
+            //Console.WriteLine($"Объект класса:{myclass.change}");
+            //Console.WriteLine($"Объект структуры:{myStruct.change}");
+
+
+            //2
+            List<Notebook> notebooks = new List<Notebook>();
+            notebooks.Add(new Notebook("MackBook 16 Pro", "Apple", 20000.00));
+            notebooks.Add(new Notebook("NoteBook", "Asus", 19000.99));
+            notebooks.Add(new Notebook("NoteBook", "Lenovo", 15000.87));
+
+            foreach (var notebook in notebooks)
+            {
+                notebook.ShowInfo();
+                Console.WriteLine(); // Добавим пустую строку для разделения информации о каждом ноутбуке
+            }
+
+            Console.ReadKey();
+
+        }
+
+        //1
+        //public static void ClassTacer(MyClass1 myClass)
+        //{
+        //    myClass.change = "изменено";
+        //}
+        //public static void StruktTacer(MyStruct myStruct)
+        //{
+        //    myStruct.change = "изменено";
+        //}
+
+    }
+}
